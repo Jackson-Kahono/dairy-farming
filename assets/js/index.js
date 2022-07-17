0c9636370dfd00f069f365018fd1d63dda5477e5
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-07-17 13:24:33 +0300

    Add total Production

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,15 +64,15 @@ add.addEventListener('click', function () {
 });
 
 function totalProduction() {
-      for (let key in sheds){
-      let row = document.createElement('tr');
-      let td1 = document.createElement('td');
-      let td2 = document.createElement('td');
-      td1.innerHTML = `Shed ${key}`;
-      td2.innerHTML = `${sheds[key]} Litres`
-      row.appendChild(td1);
-      row.appendChild(td2);
-      table.appendChild(row);
+      for (let key in sheds) {
+            let row = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            td1.innerHTML = `Shed ${key}`;
+            td2.innerHTML = `${sheds[key]} Litres`
+            row.appendChild(td1);
+            row.appendChild(td2);
+            table.appendChild(row);
       }
 }
 //function incomeOverTime(sellingPrice,time)
