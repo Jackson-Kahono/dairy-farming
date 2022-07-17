979cbb7c58a15f3572ca6167d6d997a30904a886
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-07-17 13:23:59 +0300

    Add incomeOverTime

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,16 +9,31 @@ const p = document.getElementById('p');
 //initialise variables
 let sheds = {};
 const price = 45;
+const months = {
+      'JANUARY': 31,
+      'FEBRUARY': 29,
+      'MARCH': 31,
+      'APRIL': 30,
+      'MAY': 31,
+      'JUNE': 30,
+      'JULY': 31,
+      'AUGUST': 31,
+      'SEPTEMBER': 30,
+      'OCTOBER': 31,
+      'NOVEMBER': 30,
+      'DECEMBER': 31
+}
 
-
-noOfSheds.addEventListener('keyup', function() {
+noOfSheds.addEventListener('keyup', function () {
       if (noOfSheds.value > 26) {
             noOfSheds.value = 26;
       }
       setTimeout(() => {
             generateSelect(noOfSheds.value);
-            noOfSheds.disable = true;
-      }, 6000); 
+      }, 1000);
+      setTimeout(() => {
+            noOfSheds.disabled = "1";
+      }, 3000);
 });
 
 function generateSelect(noOfSheds) {
@@ -27,34 +42,61 @@ function generateSelect(noOfSheds) {
       for (let i = 0; i < noOfSheds; i++) {
             let option = document.createElement('option');
             option.value = alphabetArray[i];
-            option.text =`shed  ${alphabetArray[i]}`;
+            option.text = `shed  ${alphabetArray[i]}`;
             select.appendChild(option);
       }
 }
 
-add.addEventListener('click', function() {
+add.addEventListener('click', function () {
       p.innerHTML = ""
-      if (noOfLitres.value && select.value != ""){
-            if(!sheds[select.value]) {
-            let row = document.createElement('tr');
-            let td1 = document.createElement('td');
-            let td2 = document.createElement('td');
-            td1.innerHTML = `Shed ${select.value}`;
-            td2.innerHTML = noOfLitres.value;
-            row.appendChild(td1);
-            row.appendChild(td2);
-            table.appendChild(row);
-            sheds[select.value] = noOfLitres.value;
-            noOfLitres.value = "";
+      if (noOfLitres.value && select.value != "") {
+            if (!sheds[select.value]) {
+                  sheds[select.value] = noOfLitres.value;
+                  noOfLitres.value = "";
             }
             else {
                   alert("Shed already exists");
                   select.value = "";
                   noOfLitres.value = "";
             }
-            
-      } 
+
+      }
 });
+
+function totalProduction() {
+      for (let key in sheds){
+      let row = document.createElement('tr');
+      let td1 = document.createElement('td');
+      let td2 = document.createElement('td');
+      td1.innerHTML = `Shed ${key}`;
+      td2.innerHTML = `${sheds[key]} Litres`
+      row.appendChild(td1);
+      row.appendChild(td2);
+      table.appendChild(row);
+      }
+}
+//function incomeOverTime(sellingPrice,time)
+function incomeOverTime(sellingPrice, time, noOfLitres) {
+      let income = 0;
+      income = sellingPrice * noOfLitres * time;
+      return income;
+}
+
+submit.addEventListener('click', function (e) {
+      e.preventDefault();
+      totalProduction();
+      let totalIncome = 0;
+      let totalNoOfLiters = 0;
+      for (let key in sheds) {
+            totalNoOfLiters += sheds[key];
+      }
+      console.log(sheds)
+      totalIncome = incomeOverTime(price, 1, totalNoOfLiters);
+});
+
+// function allYearIncome() {
+
+// }
 
 
 
